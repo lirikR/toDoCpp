f4778d14ded99f290124ba8268c18d4cff1f0811
erion <erion.halimi@student.uni-pr.edu>
2025-01-29 21:55:21 +0100

    Krijimi i ToDo Menu
    Shtimi i titullit ASCII
    Gjenereimi i menus fillestare
    Gjenerimi i prompt per menu

--- index.cpp
+++ index.cpp
@@ -15,6 +15,17 @@ struct Task {
     TaskStatus status;
 };
 
+// Printojme titullin per menu te gjeneruar me ASCII Generator
+void printTitle() {
+    cout << R"(
+___________              ________                .____    .__          __   
+\__    ___/___           \______ \   ____        |    |   |__| _______/  |_ 
+  |    | /  _ \   ______  |    |  \ /  _ \       |    |   |  |/  ___/\   __\
+  |    |(  <_> ) /_____/  |    `   (  <_> )      |    |___|  |\___ \  |  |  
+  |____| \____/          /________/\____/        |________\__/____  > |__|  
+    )" << "\n";
+}
+
 //Nr maksimal per tasks
 const int MAX_TASKS = 100;
 
@@ -26,5 +37,27 @@ int main() {
     //zgjidhja ne menu
     int choice;
 
+    do {
+        printTitle(); //Printojme titullin ASCII
+        cout << "1. Add Task\n";
+        cout << "2. Mark Task as Completed\n";
+        cout << "3. Delete Task\n";
+        cout << "4. Display Tasks\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+
+    } while (promptReturnToMenu());
+
     return 0;
+}
+
+
+//Pyesim user a deshiron te vazhdoj ne menu
+bool promptReturnToMenu() {
+    char choice;
+    cout << "\nReturn to menu? (y/n): ";
+    cin >> choice;
+    return (choice == 'y' || choice == 'Y');
 }
